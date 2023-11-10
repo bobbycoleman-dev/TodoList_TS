a41e9fb888bec795e54d7bf7ca8dd979181a35a3
Bobby Coleman <bobbycoleman.dev@gmail.com>
2023-11-10 17:15:14 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+	return (
+		<div className="h-screen dark:bg-black text-white flex flex-col items-center">
+			<h1 className="text-7xl">Todo List</h1>
+		</div>
+	);
+}
+
+export default App;
